a469b5dc47eaffee1a20398dfe15611051316bff
frank <gamalgamer10@yahoo.com>
2023-05-06 19:03:23 +0300

    added useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import { useState } from "react"
+import { useMemo, useState } from "react"
 
 function App() {
 const[numbers,setNumbers]=useState([10,20,30])
-const total = numbers.reduce((acc,number)=>acc + number,0)
+const total = useMemo(()=>numbers.reduce((acc,number)=>acc + number,0),[numbers])
+function add(){
+  setNumbers([...numbers,10])
+} 
+
+const[names,setNames]=useState(["hossam","frank","khaled","salah"])
+const sortedNames = useMemo(()=>names.sort(),[names])
+function addName(){
+  setNames([...names,"a7a"])
+}
   return (
-    <div>total : {total}</div>
+   <div> <div>total : {total}</div>
+   <button onClick={add}>add number</button>
+   <div>{sortedNames.join(", ")}</div>
+   <button onClick={addName}>add name</button>
+   </div>
+    
   )
 }
 
