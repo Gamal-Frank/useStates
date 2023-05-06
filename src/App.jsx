809385f2893ca3b522004e842fd7d364bf3c6f16
frank <gamalgamer10@yahoo.com>
2023-05-06 19:09:08 +0300

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useMemo, useState } from "react"
 
 function App() {
 const[numbers,setNumbers]=useState([10,20,30])
-const total = useMemo(()=>numbers.reduce((acc,number)=>acc + number,0),[numbers])
+const total = useMemo(()=>[...numbers].reduce((acc,number)=>acc + number,0),[numbers])
 function add(){
   setNumbers([...numbers,10])
 } 
 
 const[names,setNames]=useState(["hossam","frank","khaled","salah"])
-const sortedNames = useMemo(()=>names.sort(),[names])
+const sortedNames = useMemo(()=>[...names].sort(),[names])
 function addName(){
   setNames([...names,"a7a"])
 }
